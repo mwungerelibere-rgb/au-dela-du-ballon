c020d2db86295d4927d1308e47500f0761417d58
LIBERE <mwungerelibere@gmail.com>
2026-08-03 05:38:24 +0200

    Fix news cache

--- service-worker.js
+++ service-worker.js
@@ -1,73 +1,51 @@
-const CACHE_NAME = "beyond-ball-v2";
+const CACHE_NAME = "beyond-ball-v3";
 
 const FILES = [
     "index.html",
     "dashboard.html",
     "archive.html",
-    "news.json",
     "manifest.json"
 ];
 
-
+// Install
 self.addEventListener("install", event => {
-
     event.waitUntil(
-
-        caches.open(CACHE_NAME)
-        .then(cache => {
-
-            return cache.addAll(FILES);
-
-        })
-
+        caches.open(CACHE_NAME).then(cache => cache.addAll(FILES))
     );
-
 });
 
-
-
+// Activate
 self.addEventListener("activate", event => {
-
     event.waitUntil(
-
-        caches.keys()
-        .then(keys => {
-
-            return Promise.all(
-
+        caches.keys().then(keys =>
+            Promise.all(
                 keys.map(key => {
-
-                    if(key !== CACHE_NAME){
-
+                    if (key !== CACHE_NAME) {
                         return caches.delete(key);
-
                     }
-
                 })
-
-            );
-
-        })
-
+            )
+        )
     );
-
 });
 
-
-
+// Fetch
 self.addEventListener("fetch", event => {
 
+    // Never cache news.json
+    if (event.request.url.includes("news.json")) {
+
+        event.respondWith(
+            fetch(event.request)
+                .catch(() => caches.match(event.request))
+        );
+
+        return;
+    }
 
     event.respondWith(
-
-        caches.match(event.request)
-        .then(response => {
-
+        caches.match(event.request).then(response => {
             return response || fetch(event.request);
-
         })
-
     );
-
-
 });
